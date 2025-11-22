baf39a173557b56a3f0207d31b54d8d243e0398d
Dejaunte <500001483@my.fiveislands.uwi.edu>
2025-11-22 13:49:05 -0400

    completed exercise 4e

--- world.php
+++ world.php
@@ -11,15 +11,29 @@ if (isset($_GET['country'])){
   $country = $_GET['country'];
   $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
-}else{
+}else {
 $stmt = $conn->query("SELECT * FROM countries");
 }
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
+<table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Continent</th>
+      <th>Independence</th>
+      <th>Head of State</th>
+    </tr>
+  </thead>
+  <tbody>
 <?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+  <td><?= $row['name'] ?></td>
+  <td><?= $row['continent']; ?></td>
+  <td><?= $row['independence_year']; ?></td>
+  <td><?= $row['head_of_state']; ?></td>
+</tr>
 <?php endforeach; ?>
-</ul>
+</tbody>
+</table>
